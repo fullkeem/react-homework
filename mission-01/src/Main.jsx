325034d74a50aa82095479910bcfb4fbf47b91ca
eddiekeem <cm730712@gmail.com>
2024-01-22 02:30:34 +0900

    파일 위치 변경

--- mission-01/src/Main.jsx
+++ mission-01/src/Main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header";
-import { Login } from "./login";
+import { Login } from "./pages/Login";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
