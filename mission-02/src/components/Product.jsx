c1588692262d559d1c374f1dd38a45a0c332896a
KIMCHOONGMAN <cm730712@gmail.com>
2024-01-28 01:06:41 +0900

    map을 이용한 ProductList 렌더링 구현

--- mission-02/src/components/Product.jsx
+++ mission-02/src/components/Product.jsx
@@ -1,14 +1,18 @@
-import productListData from '../data/productListData.json';
+import ProductListData from '../data/productListData.json';
 import ProductList from './ProductList';
 
 export default function Product() {
-  console.log(productListData);
+  console.log(ProductListData.products);
   return (
     <section className="flex items-center justify-center w-full">
       <div className="flex justify-between h-24pxr mb-20pxr">
         <h1 className="sr-only">상품리스트</h1>
         <div className="w-783pxr">
-          <ul className="flex flex-wrap justify-between product-container"></ul>
+          <ul className="flex flex-wrap justify-between product-container">
+            {ProductListData.products.map((data) => (
+              <ProductList key={data.id} {...data} />
+            ))}
+          </ul>
         </div>
       </div>
     </section>
